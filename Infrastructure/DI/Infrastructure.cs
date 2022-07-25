fbdf17c85334522dcd174f99101b12a4918080ba
A.Sami <wow1.devill@gmail.com>
2022-07-25 16:48:01 +0430

    Initial the Db Context

---
+++ Infrastructure/DI/Infrastructure.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DI;
+
+public static class Infrastructure
+{
+    public static IServiceCollection InfrastructureDi(this IServiceCollection service, IConfiguration config)
+    {
+        service.AddTransient<IUnitOfWork>();
+        service.AddDbContext<ApplicationDbContext>
+        (option => option.UseSqlServer(config.GetConnectionString("Death"), _ =>
+        {
+            _.EnableRetryOnFailure(3);
+            _.CommandTimeout(1400);
+        }));
+
+        return service;
+    }
+}
